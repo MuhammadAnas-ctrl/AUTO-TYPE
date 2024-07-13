f5fc97b3064bf13b22e8bf7a6bc78742e4e703e5
ANAS <flashycoderch@gmail.com>
2024-07-13 14:20:29 +0500

    Javascript Auto Type

---
+++ index.js
@@ -0,0 +1,6 @@
+let typed = new TypeError(".auto-type",{
+    strings : ["Programmer" , "Designer" , "Youtuber","Programmer" , "Designer" , "Youtuber","Programmer" , "Designer" , "Youtuber","Programmer" , "Designer" , "Youtuber"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    looped : true
+})
